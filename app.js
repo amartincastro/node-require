dba2f6f615c73d2baa7f34981be19b9968f0e693
amartincastro <castroarielm@gmail.com>
2018-06-15 10:44:02 -0700

    clean up comments, config nodemon

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
+const _ = require('lodash');
+
 const notes = require('./notes.js');
-
-var res = notes.addNote();
-console.log(res);
-
-console.log('Results:', notes.add(9,-2));
-
-// var user = os.userInfo();
-//
-// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
