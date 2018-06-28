ecc927c6ee7763709e311ae0656f3233377d7ebf
amartincastro <castroarielm@gmail.com>
2018-06-28 15:34:05 -0700

    refactor yargs title / body commands and aliases

--- app.js
+++ app.js
@@ -4,18 +4,29 @@ const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-const argv = yargs
-.command('add', 'Add a new note.', {
-  title: {
+const titleOptions = {
     describe: 'Title of note.',
     demand: true,
     alias: 't'
-  },
-  body: {
+};
+
+const bodyOptions = {
     describe: 'Body of note.',
     demand: true,
     alias: 'b'
   }
+
+const argv = yargs
+.command('add', 'Add a new note.', {
+  title: titleOptions,
+  body: bodyOptions
+})
+.command('list', 'List all notes')
+.command('read', 'Read a note', {
+  title: titleOptions
+})
+.command('remove', 'Remove a note', {
+  title: titleOptions
 })
 .help()
 .argv;
