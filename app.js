c70025159b811e2613c0b4636c8c279ba34775b7
amartincastro <castroarielm@gmail.com>
2018-06-28 14:58:04 -0700

    fix forEach to pass in note

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ if (command === 'add') {
 } else if (command === 'list') {
   var allNotes = notes.getAll();
   console.log(`Printing ${allNotes.length} note(s).`);
-  allNotes.forEach(() => notes.logNote(note));
+  allNotes.forEach((note) => notes.logNote(note));
 } else if (command === 'read') {
   var note = notes.getNote(argv.title);
   if (note) {
